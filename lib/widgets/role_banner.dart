516be10c05c6df85f7151f4dc813e409cf1b79d5
IAmThe2ndHuman <kerimkoray.ozturkler1@gmail.com>
2022-07-07 18:26:11 +0300

    Updated role reward box

--- lib/widgets/role_banner.dart
+++ lib/widgets/role_banner.dart
@@ -2,35 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class RoleBanner extends StatelessWidget {
+  final int level;
   final String name;
   final Color color;
+  List<Color> get gradient => [color.withOpacity(0.5), color];
 
-  const RoleBanner(this.name, this.color, {Key? key}) : super(key: key);
+  const RoleBanner({Key? key, required this.level, required this.name, required this.color}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 10),
-      child: Container(
+    return Container(
+        padding: const EdgeInsets.all(16),
+        margin: const EdgeInsets.symmetric(vertical: 5),
         decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.centerLeft,
-            end: Alignment.centerRight,
-            colors: [
-              color,
-              color.withOpacity(0.4)
-            ]
-          ),
-          borderRadius: const BorderRadius.all(Radius.circular(30)),
+            borderRadius: BorderRadius.circular(10),
+            gradient: LinearGradient(colors: gradient)
         ),
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Align(
-            alignment: Alignment.center,
-            child: Text(name, style: GoogleFonts.inter(fontWeight: FontWeight.bold)),
-          ),
-        ),
-      ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(name, style: const TextStyle(fontWeight: FontWeight.bold, letterSpacing: 1.5, /*fontFamily: "Roboto"*/)),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(99)
+              ),
+              child: Text("LV $level", style: GoogleFonts.inter(color: Colors.black),),
+            )
+          ],
+        )
     );
   }
 }
--- lib/widgets/role_rewards.dart
+++ lib/widgets/role_rewards.dart
@@ -1,39 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:hee8_lb/widgets/role_banner.dart';
 
 class RoleRewards extends StatelessWidget {
   const RoleRewards({Key? key}) : super(key: key);
 
+  final Color _bgColor = const Color(0xFF21252b);
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      alignment: Alignment.centerLeft,
-      decoration: const BoxDecoration(
-        color: Color(0xff21262B),
+      width: 300,
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: _bgColor,
         boxShadow: [
           BoxShadow(
             color: Colors.black,
             offset: Offset(1.0, 1.0),
             blurRadius: 6.0,
           ),
+        ]
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("ROLE REWARDS", style: GoogleFonts.inter(fontWeight: FontWeight.bold, letterSpacing: 2, fontSize: 15)),
+              Tooltip(
+                  preferBelow: false,
+                  verticalOffset: 30,
+                  textStyle: TextStyle(color: Colors.white, fontSize: 15, fontWeight: FontWeight.normal),
+                  decoration: BoxDecoration(
+                      color: Colors.grey[900],
+                      borderRadius: BorderRadius.circular(5)
+                  ),
+                  message: "Send messaqes in the server to qain XP and receive roles!",
+                  child: const Icon(Icons.help, size: 17, color: Colors.grey)
+              )
+            ],
+          ),
+          const SizedBox(height: 5),
+          const RoleBanner(
+            level: 10,
+            name: "H Noble",
+            color: Color(0xFF433bff),
+          ),
+          const RoleBanner(
+            level: 20,
+            name: "H Novice",
+            color: Color(0xFF6d59ff),
+          ),
+          const RoleBanner(
+            level: 30,
+            name: "H Pro",
+            color: Color(0xFF7d8bff),
+          ),
+          const RoleBanner(
+            level: 40,
+            name: "H Master",
+            color: Color(0xFF7f8ed5),
+          ),
+          const RoleBanner(
+            level: 50,
+            name: "H Leqend",
+            color: Color(0xFFfc09f5),
+          ),
         ],
       ),
-      height: 400,
-      width: 300,
-      child: Padding(
-        padding: const EdgeInsets.all(15),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
-            Text("Role Rewards", style: TextStyle(fontSize: 20)),
-            RoleBanner("H Noble (10)", Color(0xFF433BFF)),
-            RoleBanner("H Novice (20)", Color(0xFF6D59FF)),
-            RoleBanner("H Pro (30)", Color(0xFF7D8BFF)),
-            RoleBanner("H Master (40)", Color(0xFF7F8ED5)),
-            RoleBanner("H Leqend (50)", Color(0xFFFC09F5))
-          ],
-        ),
-      )
     );
   }
 }
