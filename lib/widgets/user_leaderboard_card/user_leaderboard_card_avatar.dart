ad7527cb1ea6c7a5dea89ffee460a1213d3b06c7
emirsurmen <emirsurmen@gmail.com>
2022-07-19 13:43:25 +0300

    make avatar xp ring smaller and make the animation longer

--- lib/widgets/user_leaderboard_card/user_leaderboard_card_avatar.dart
+++ lib/widgets/user_leaderboard_card/user_leaderboard_card_avatar.dart
@@ -26,16 +26,19 @@ class UserLeaderboardCardAvatar extends StatelessWidget {
       padding: padding ?? const EdgeInsets.only(left: 20, right: 20),
       child: CircularPercentIndicator(
         radius: 30,
+        lineWidth: 3,
         backgroundColor: const Color.fromARGB(255, 44, 49, 54),
         percent: Utils.calculatePercentage(xp, level),
         animation: true,
         circularStrokeCap: CircularStrokeCap.round,
+        curve: Curves.easeInOutCubic,
+        animationDuration: 1000,
         center: ClipRRect(
           borderRadius: BorderRadius.circular(99),
           child: Image.network(
             avatarUrl,
-            height: height ?? 50,
-            width: width ?? 50,
+            height: height ?? 52,
+            width: width ?? 52,
             loadingBuilder: (context, child, loadingProgress) {
               if (loadingProgress == null) {
                 return child;
