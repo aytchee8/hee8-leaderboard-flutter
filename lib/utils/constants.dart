fc3f236fd22bd068cd02bfc57327270b109d6d9a
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2022-08-26 22:22:56 +0100

    update api endpoint

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -4,5 +4,4 @@ const kIBMPlexSans = "IBMPlexSans";
 
 const kLocalBaseAPIUrl = "http://localhost:8080";
 const kTestBaseAPIUrl = "https://queue-tapes-produce-keep.trycloudflare.com";
-const kMainBaseAPIUrl = "https://api.hee8bot.xyz";
-const kIpBaseAPIUrl = "http://144.172.75.148:8080";
+const kMainBaseAPIUrl = "https://hee8bot.tk";
